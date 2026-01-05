4f776effd0ecf7b0f561439530e55f0067ad91f1
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-05 22:39:56 +0530

    Implement digital clock logic with AM/PM and 12/24 hour toggle

---
+++ script.js
@@ -0,0 +1,44 @@
+const hourSpan = document.querySelector(".hour");
+const minSpan = document.querySelector(".min");
+const secSpan = document.querySelector(".sec");
+const formatBtn = document.querySelector(".format");
+const ampmSpan = document.querySelector(".am");
+
+let is24Hour = true;
+
+const timer = () => {
+  const now = new Date();
+
+  let hour24 = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+
+  // AM / PM decision (from 24-hour time)
+  let ampm = hour24 >= 12 ? "PM" : "AM";
+
+  let displayHour = hour24;
+
+  // format logic
+  if (!is24Hour) {
+    displayHour = hour24 % 12 || 12;
+    ampmSpan.innerText = ampm;
+    ampmSpan.style.display = "inline";
+    formatBtn.innerText = "Switch to 24 Hour";
+  } else {
+    ampmSpan.style.display = "none";
+    formatBtn.innerText = "Switch to 12 Hour";
+  }
+
+  hourSpan.innerText = displayHour < 10 ? "0" + displayHour : displayHour;
+  minSpan.innerText = min < 10 ? "0" + min : min;
+  secSpan.innerText = sec < 10 ? "0" + sec : sec;
+};
+
+// button only flips state
+formatBtn.addEventListener("click", () => {
+  is24Hour = !is24Hour;
+  timer(); // instant update
+});
+
+timer();
+setInterval(timer, 1000);
